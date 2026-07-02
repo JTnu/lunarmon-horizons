764ab44669e91056d0fea461934dc9c72f99653f
MistahLay <clarencetoledo08@gmail.com>
2026-07-02 17:35:34 +0800

    Added id factory helper

--- src/main/java/lunarmonhorizons/LunarMonHorizons.java
+++ src/main/java/lunarmonhorizons/LunarMonHorizons.java
@@ -6,12 +6,20 @@ import lunarmonhorizons.item.ModItemGroups;
 import lunarmonhorizons.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LunarMonHorizons implements ModInitializer {
 	public static final String MOD_ID = "lunarmon-horizons";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	/**
+	 * Helper method to create Mod's ids
+	 */
+	public static ResourceLocation of(String path){
+		return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
+	}
 
 	@Override
 	public void onInitialize() {
