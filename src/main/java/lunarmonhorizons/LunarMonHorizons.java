fbc3eac7fad72778b83cf2393c186e1cc23ad0c1
MistahLay <clarencetoledo08@gmail.com>
2026-07-02 20:05:22 +0800

    Added Custom GameRule

--- src/main/java/lunarmonhorizons/LunarMonHorizons.java
+++ src/main/java/lunarmonhorizons/LunarMonHorizons.java
@@ -5,14 +5,20 @@ import lunarmonhorizons.block.entity.ModBlockEntities;
 import lunarmonhorizons.item.ModItemGroups;
 import lunarmonhorizons.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LunarMonHorizons implements ModInitializer {
 	public static final String MOD_ID = "lunarmon-horizons";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static GameRules.Key<GameRules.BooleanValue> SCAN_ANCHORS = GameRuleRegistry
+			.register("scan_anchors", GameRules.Category.UPDATES, GameRuleFactory.createBooleanRule(false));
 
 	/**
 	 * Helper method to create Mod's ids
