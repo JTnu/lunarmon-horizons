765d1972e21a1723495709aebcbda90433baa347
MistahLay <clarencetoledo08@gmail.com>
2026-07-04 17:00:20 +0800

    TODO

--- src/main/java/lunarmonhorizons/LunarMonHorizonsDataGenerator.java
+++ src/main/java/lunarmonhorizons/LunarMonHorizonsDataGenerator.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class LunarMonHorizonsDataGenerator implements DataGeneratorEntrypoint {
+	// TODO: Check out kaupenjoe's data gen, should be simple enough for creating models
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
