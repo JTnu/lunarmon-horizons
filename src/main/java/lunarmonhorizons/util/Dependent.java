67031746199aa235b46c8a6dbfaad3860880eb1c
MistahLay <clarencetoledo08@gmail.com>
2026-07-04 16:59:15 +0800

    A potential way to manage multiple requirements

---
+++ src/main/java/lunarmonhorizons/util/Dependent.java
@@ -0,0 +1,10 @@
+package lunarmonhorizons.util;
+
+/**
+ * TODO: A coherent system where a class can have multiple dependency to be checked pre/mid execution
+ */
+public interface Dependent {
+
+    boolean isFulfilled();
+
+}
